8ad031ef719750b0402ad8de3b637417e3705226
shanu31396 <shanu31396@gmail.com>
2025-04-13 06:17:16 +0530

    first commit

---
+++ 01_INTRO_TO_C++.cpp
@@ -0,0 +1,10 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Hello World!" << endl;
+    // std::cout << "Hello World!" << std::endl;
+    return 0;
+}
