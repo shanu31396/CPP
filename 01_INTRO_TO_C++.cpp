70fdc4bcb399266ba3d5b64624c4b34510f5be64
shanu31396 <shanu31396@gmail.com>
2025-04-13 07:35:07 +0530

    2nd commit

--- 01_INTRO_TO_C++.cpp
+++ 01_INTRO_TO_C++.cpp
@@ -1,10 +1,20 @@
-//#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 #include <iostream>
 
 using namespace std;
 int main()
 {
+    int a = 1;
+    float b = 1.1;
+    char c = 'a';
     cout << "Hello World!" << endl;
     // std::cout << "Hello World!" << std::endl;
+    cout << "The value of a is " << a << endl;
+    cout << "The value of b is " << b << endl;
+    cout << "The value of c is " << c << endl;
     return 0;
 }
+
+/*The entire code is pushed on
+ GitHub having address*/
+// https://github.com/shanu31396/CPP
