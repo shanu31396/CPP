a4c9ef4213f08ebea0bee7b60569711b53fc3683
shanu31396 <shanu31396@gmail.com>
2025-04-29 10:45:55 +0530

    swap

---
+++ swap.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int swapR(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+int swapR2(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "Inside swapR2: a = " << a << ", b = " << b << endl;
+}
+
+int &swapRef(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    return b;
+}
+int main()
+{
+    int a = 1, b = 2;
+    cout << "Before swap: a = " << a << ", b = " << b << endl;
+    swap(&a, &b);
+    cout << "After swap: a = " << a << ", b = " << b << endl;
+    swapR(a, b);
+    cout << "After swapR: a = " << a << ", b = " << b << endl;
+    swapR2(a, b);
+    cout << "After swapR2: a = " << a << ", b = " << b << endl;
+
+    swapRef(a, b) = 9;
+    cout << "After swapRef: a = " << a << ", b = " << b << endl;
+    return 0;
+}
